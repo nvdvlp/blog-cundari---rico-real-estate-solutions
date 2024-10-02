c8a94e2421ce6b66794b66f8bcd0ca79a941152a
Santiago Nieves <137436428+nvdvlp@users.noreply.github.com>
2024-10-02 14:29:37 -0500

    page not found 404 created

---
+++ blog-app/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function NotFound(){
+    return <section>
+        <h1>404</h1>
+        <p>Page Not Found</p>
+        <Link href="/">
+            Volver
+        </Link>
+    </section>
+}
