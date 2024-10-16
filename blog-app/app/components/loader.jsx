c1f4b63d7d1a7c5806c83edd58cb34502e5a457e
Santiago Nieves <137436428+nvdvlp@users.noreply.github.com>
2024-10-16 14:22:00 -0500

    mobile createPost

--- blog-app/app/components/loader.jsx
+++ blog-app/app/components/loader.jsx
@@ -1,6 +1,23 @@
+'use client';
+import { useState, useEffect } from 'react';
 import '../css/loader.css';
 
 export default function Loader() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (!isLoading) {
+        return null;
+    }
+
     return (
         <section className='loaderContainer'>
             <span className="loader"></span>   
