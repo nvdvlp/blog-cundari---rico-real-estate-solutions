c178b8ca294880f543c15d5726f526125be93a99
Santiago Nieves <137436428+nvdvlp@users.noreply.github.com>
2024-10-22 12:19:29 -0500

    a lot of changes v3

--- blog-app/app/layout.js
+++ blog-app/app/layout.js
@@ -1,26 +1,25 @@
 
-'use client'
 
 import './css/global.css'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import Script from 'next/script';
-import Loader from './components/loader';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
 
 
-// export const metadata = {
-//   title: 'Cundari & Rico Real Estate Solutions',
-//   description: 'Search available properties for sale or lease',
-//   keywords: [
-//     'Homes for rent',
-//     'Real estate market trends',
-//     'Commercial properties for sale',
-//     'Real estate agent services',
-//     'Property management services'
-//   ]
-// }
+export const metadata = {
+  title: 'Cundari & Rico Real Estate Solutions',
+  description: 'Search available properties for sale or lease',
+  keywords: [
+    'Homes for rent',
+    'Real estate market trends',
+    'Commercial properties for sale',
+    'Real estate agent services',
+    'Property management services'
+  ]
+}
 
 const inter = Inter({
   weight:["300", "400", "500", "700"],
@@ -29,25 +28,25 @@ const inter = Inter({
 })
 
 export default function RootLayout({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [loading, setLoading] = useState(true);
+  // const router = useRouter();
 
-  useEffect(() => {
-    const token = localStorage.getItem('authID'); 
-    if (token) {
-      setIsAuthenticated(true); 
-    } else {
-      if (router.pathname !== '/login') {
-        router.push('/login'); // Redirige a login si no está autenticado y no está en la página de login
-      }
-    }
-    setLoading(false); 
-  }, [router]);
+  // useEffect(() => {
+  //   const token = localStorage.getItem('authID'); 
+  //   if (token) {
+  //     setIsAuthenticated(true); 
+  //   } else {
+  //     if (router.pathname !== '/login') {
+  //       router.push('/login'); // Redirige a login si no está autenticado y no está en la página de login
+  //     }
+  //   }
+  //   setLoading(false); 
+  // }, [router]);
 
-  if (loading) {
-    return <Loader />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   return (
     <html lang="en">
